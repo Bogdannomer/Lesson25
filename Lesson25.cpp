ecc2ba4be1a59833c11996f404ac33bba106ddab
Bogdan <smetiuch12@gmail.com>
2022-05-14 09:31:05 +0300

    git reset

--- Lesson25.cpp
+++ Lesson25.cpp
@@ -20,3 +20,4 @@ int main()
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
 //111111111111111111111111111111
 //222222222222222222222222222222
+//--hard
